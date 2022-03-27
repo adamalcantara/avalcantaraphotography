71b2f42030c340df5e7b187b34a63e342cbb1004
adamalcantara <alcantaradevelopment@gmail.com>
2022-03-27 14:56:39 -0400

    Fixed routing issue by downgrading react-router-dom, made some styling fixes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Navbar from "./Components/Navbar/Navbar"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
 
-// import the pages
-import Home from "./Pages/Home/Home";
-import About from "./Pages/About/About";
-import Contact from "./Pages/Contact/Contact"
-import Portraits from "./Pages/Portraits/Portraits";
+//Import all the pages
 import Headshots from "./Pages/Headshots/Headshots";
 
 function App() {
   return (
     <Router>
       <div className="App">
-
         <Navbar />
-        <div className='pageContent'>
-          {/* <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/contact" component={Contact} />
-          <Route exact path="/portraits" component={Portraits} />
-          <Route exact path="/headshots" component={Headshots} /> */}
-        </div>
+        
+          
+            <Route exact path="/headshots" component={Headshots}/>
+          
+        
+        
+        
       </div>
     </Router>
+
   );
 }
 
