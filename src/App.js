6eb8a51ea4da15b69d6559b5cea9daddc8234ffb
adamalcantara <alcantaradevelopment@gmail.com>
2022-03-27 15:02:54 -0400

    Routes added, default code added to all pages, routes working.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
 
 //Import all the pages
+import Home from "./Pages/Home/Home"
+import About from "./Pages/About/About";
+import Contact from "./Pages/Contact/Contact";
+import Portraits from "./Pages/Portraits/Portraits";
 import Headshots from "./Pages/Headshots/Headshots";
 
 function App() {
@@ -11,7 +15,10 @@ function App() {
       <div className="App">
         <Navbar />
         
-          
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/contact" component={Contact} />
+            <Route exact path="/portraits" component={Portraits} />
             <Route exact path="/headshots" component={Headshots}/>
           
         
