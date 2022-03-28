7d39f6f9d34de2bba3ea1a1f567ee68e732fd584
adamalcantara <alcantaradevelopment@gmail.com>
2022-03-28 17:16:25 -0400

    import images

--- src/Pages/Headshots/Headshots.js
+++ src/Pages/Headshots/Headshots.js
@@ -1,8 +1,13 @@
 import React, {useState} from 'react';
 import './Headshots.css';
-import CgClose from 'react-icons/cg'
+import CgClose from 'react-icons/cg';
 
-
+// import images
+import Img1 from '../../img/headshots/1.jpg';
+import Img2 from '../../img/headshots/2.jpg';
+import Img3 from '../../img/headshots/3.jpg';
+import Img4 from '../../img/headshots/4.jpg';
+import Img5 from '../../img/headshots/5.jpg';
 
 
  const Headshots = () => {
