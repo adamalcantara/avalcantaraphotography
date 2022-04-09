8fe7400acb5c35f4837d34958e4c9d17b8fbf88f
adamalcantara <alcantaradevelopment@gmail.com>
2022-04-08 20:05:26 -0400

    Styled home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,7 +4,7 @@ import "./Home.css"
 const Home = () => {
   return (
     <div>
-      <div>
+      <div id="introDiv">
           <p id="weAre">We are Adam and Valerie Alcantara, a husband and wife photography team located in Orlando Florida! We specialize in headshots, portraits, and weddings.</p>
           <button id="learnMore">Click Here To Learn More</button>
       </div>
