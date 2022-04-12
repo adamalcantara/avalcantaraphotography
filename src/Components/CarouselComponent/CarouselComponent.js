be441241ceb9e30c9834504bc7a607d512d055cb
adamalcantara <alcantaradevelopment@gmail.com>
2022-04-12 13:31:41 -0400

    Carousel customization added

--- src/Components/CarouselComponent/CarouselComponent.js
+++ src/Components/CarouselComponent/CarouselComponent.js
@@ -3,6 +3,7 @@ import { Carousel } from 'react-responsive-carousel';
 
 // Carousel CSS
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "./CarouselComponent.css"
 
 import slider1 from "../../img/slider/1.jpg"
 import slider2 from "../../img/slider/2.jpg"
@@ -11,10 +12,12 @@ import slider4 from "../../img/slider/4.jpg"
 import slider5 from "../../img/slider/5.jpg"
 import slider6 from "../../img/slider/6.jpg"
 
+
+
 function CarouselComponent() {
     return (
         <div className="carousel-wrapper">
-            <Carousel infiniteLoop useKeyboardArrows autoPlay>
+            <Carousel infiniteLoop useKeyboardArrows autoPlay showThumbs={false} showStatus={false} interval={4000} transitionTime={450} animationHandler="fade" width="50%">
                 <div>
                     <img src={slider1} />
                 </div>
