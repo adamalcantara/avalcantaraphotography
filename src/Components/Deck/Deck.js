e17a768b234dca54f33315eec0f1a1cca32fa79c
adamalcantara <alcantaradevelopment@gmail.com>
2022-04-12 11:14:23 -0400

    Card imported, constructor added

--- src/Components/Deck/Deck.js
+++ src/Components/Deck/Deck.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react'
 
+import Card from "../../Components/Card/Card"
+
 class Deck extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         
+      }
+    }
   render() {
     return (
       <div>
