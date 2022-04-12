38b0c0ba358c2166a87033baa5cc054e86609558
adamalcantara <alcantaradevelopment@gmail.com>
2022-04-12 13:08:40 -0400

    New carousel code added from react-responsive-carousel

---
+++ src/Components/Carousel/Carousel.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Carousel() {
+    return (
+        <div>
+            <h3>Carousel in React</h3>
+        </div>
+    );
+}
+
+export default Carousel
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,13 +2,12 @@ import React from 'react'
 import { Link } from "react-router-dom"
 import "./Home.css"
 
-// Import Slider pieces
-import Deck from "../../Components/Deck/Deck"
+import Carousel from "../../Components/Carousel/Carousel"
 
 const Home = () => {
   return (
     <div>
-      <Deck />
+      <Carousel />
       <div id="introDiv">
           <p id="weAre">We are Adam and Valerie Alcantara, a husband and wife photography team located in Orlando Florida! We specialize in headshots, portraits, and weddings.</p>
           <Link to="/about" id="learnMore">Learn More</Link>
