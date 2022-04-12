2da981b7631e65b87f56232afbc8903121034f2d
adamalcantara <alcantaradevelopment@gmail.com>
2022-04-12 11:13:35 -0400

    New starter code for slider added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Link } from "react-router-dom"
 import "./Home.css"
-import Slider from "../../Components/Slider/Slider"
+
+// Import Slider pieces
+import Deck from "../../Components/Deck/Deck"
 
 const Home = () => {
   return (
     <div>
       <div id="introDiv">
-        <Slider />
           <p id="weAre">We are Adam and Valerie Alcantara, a husband and wife photography team located in Orlando Florida! We specialize in headshots, portraits, and weddings.</p>
           <Link to="/about" id="learnMore">Learn More</Link>
       </div>
