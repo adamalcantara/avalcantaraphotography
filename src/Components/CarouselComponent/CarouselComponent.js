73a80f8aa9cd9820030382482e68b528cf0ad374
adamalcantara <alcantaradevelopment@gmail.com>
2022-04-12 13:12:38 -0400

    Carousel functioning

---
+++ src/Components/CarouselComponent/CarouselComponent.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Carousel } from 'react-responsive-carousel';
+
+// Carousel CSS
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+import slider1 from "../../img/slider/1.jpg"
+import slider2 from "../../img/slider/2.jpg"
+import slider3 from "../../img/slider/3.jpg"
+import slider4 from "../../img/slider/4.jpg"
+import slider5 from "../../img/slider/5.jpg"
+import slider6 from "../../img/slider/6.jpg"
+
+function CarouselComponent() {
+    return (
+        <div className="carousel-wrapper">
+            <Carousel infiniteLoop useKeyboardArrows autoPlay>
+                <div>
+                    <img src={slider1} />
+                </div>
+                <div>
+                    <img src={slider2} />
+                </div>
+                <div>
+                    <img src={slider3} />
+                </div>
+            </Carousel>
+        </div>
+    );
+}
+
+export default CarouselComponent
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from "react-router-dom"
 import "./Home.css"
 
-import Carousel from "../../Components/Carousel/Carousel"
+import Carousel from "../../Components/CarouselComponent/CarouselComponent"
 
 const Home = () => {
   return (
