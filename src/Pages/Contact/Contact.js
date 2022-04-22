0dc75d7de9121eab1684de8606a92010303eb185
adamalcantara <alcantaradevelopment@gmail.com>
2022-04-22 11:14:27 -0400

    Code updated to remove image

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import ContactForm from "../../Components/ContactForm/ContactForm"
 import "./Contact.css"
-import bowling from "../../img/bowling.jpg"
+
 
 const Contact = () => {
   return (
     <div id="contactPage">
-      {/* <img src={bowling} id="bowling" alt="contact us"></img> */}
+      
       <ContactForm />
     </div>
   )
