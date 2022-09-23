ce2215b956e885c067fff667406d5743b04b9ae5
adamalcantara <alcantaradevelopment@gmail.com>
2022-09-23 10:18:41 -0400

    Updated footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <div id="footer">
         <p>&copy; 2022 A.V. Alcantara Photography</p>
-        <p>Website development by <a href="https://alcantaradevelopment.com/" target="_blank" rel="noopener noreferrer" id="devLink">Alcantara Development</a></p>
+        <p>Website by <a href="https://alcantaradevelopment.com/" target="_blank" rel="noopener noreferrer" id="devLink">Alcantara Development</a></p>
     </div>
   )
 }
